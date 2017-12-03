8034a802d639c2701024c3118b873b996c042c2a
DianaArapova <diiana_arapova@mail.ru>
2017-12-03 23:39:02 +0500

    fixed

--- TagsCloudVisualization/CircularCloud/CloudLayouter/CircularCloudLayouter.cs
+++ TagsCloudVisualization/CircularCloud/CloudLayouter/CircularCloudLayouter.cs
@@ -7,7 +7,7 @@ namespace TagsCloudVisualization.CircularCloud.CloudLayouter
 	public class CircularCloudLayouter : ICircularCloudLayouter
 	{
 		public readonly Point Center;
-		private readonly IRectanglePlacer rectanglePlacer;
+		private readonly Func<IRectanglePlacer> rectanglePlacerFunc;
 		
 		public CircularCloudLayouter(Config congig, Func<IRectanglePlacer> rectanglePlacer)
 		{
@@ -15,11 +15,12 @@ namespace TagsCloudVisualization.CircularCloud.CloudLayouter
 			if (center.X < 0 || center.Y < 0)
 				throw new ArgumentException("Coordinat of center is negative");
 			Center = center;
-			this.rectanglePlacer = rectanglePlacer.Invoke();
+			rectanglePlacerFunc = rectanglePlacer;
 		}
 
 		public Rectangle PutNextRectangle(Size rectangleSize)
 		{
+			var rectanglePlacer = rectanglePlacerFunc.Invoke();
 			if (rectangleSize.Height < 0 || rectangleSize.Width < 0)
 				throw new ArgumentException("Size of rectangle is negative");
 			return rectanglePlacer.FindLocationForRectangle(rectangleSize);
